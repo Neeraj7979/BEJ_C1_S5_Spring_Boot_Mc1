5712b03c4c2c4e7267c30348d25e439f7c67f61d
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-27 23:32:23 +0530

    Added dependency injection to productRepository and autowired the constructor

--- src/main/java/com/niit/bej/c1_s5_spring_boot_Mc1/service/ProductServiceImpl.java
+++ src/main/java/com/niit/bej/c1_s5_spring_boot_Mc1/service/ProductServiceImpl.java
@@ -1,12 +1,20 @@
 package com.niit.bej.c1_s5_spring_boot_Mc1.service;
 
+import com.niit.bej.c1_s5_spring_boot_Mc1.ProductRepository;
 import com.niit.bej.c1_s5_spring_boot_Mc1.domain.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+    ProductRepository productRepository;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 
     @Override
     public List<Product> getAllProduct() {
