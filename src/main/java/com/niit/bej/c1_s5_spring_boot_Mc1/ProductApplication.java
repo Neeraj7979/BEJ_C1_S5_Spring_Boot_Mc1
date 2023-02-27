d5b95d47744610d96286b770e0ce4da2dcfc3583
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-27 23:12:00 +0530

    Initial commit

---
+++ src/main/java/com/niit/bej/c1_s5_spring_boot_Mc1/ProductApplication.java
@@ -0,0 +1,13 @@
+package com.niit.bej.c1_s5_spring_boot_Mc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+
+}
