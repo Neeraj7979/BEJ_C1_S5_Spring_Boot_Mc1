1d73d568c2358bd46bfb632fd05e12cd9596e73c
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-27 23:30:55 +0530

    Created class productServiceImpl in the package services and implemented productService Interface

---
+++ src/main/java/com/niit/bej/c1_s5_spring_boot_Mc1/service/ProductServiceImpl.java
@@ -0,0 +1,20 @@
+package com.niit.bej.c1_s5_spring_boot_Mc1.service;
+
+import com.niit.bej.c1_s5_spring_boot_Mc1.domain.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Override
+    public List<Product> getAllProduct() {
+        return null;
+    }
+
+    @Override
+    public Product addProduct(Product product) {
+        return null;
+    }
+}
