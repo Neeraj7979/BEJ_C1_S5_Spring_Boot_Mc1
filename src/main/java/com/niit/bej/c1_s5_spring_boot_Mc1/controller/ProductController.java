a03d39b514197d2ee0a5b09b327916c5fb28f8d6
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-27 23:47:38 +0530

    Created PostMapping method to respond to the post request

--- src/main/java/com/niit/bej/c1_s5_spring_boot_Mc1/controller/ProductController.java
+++ src/main/java/com/niit/bej/c1_s5_spring_boot_Mc1/controller/ProductController.java
@@ -1,8 +1,16 @@
 package com.niit.bej.c1_s5_spring_boot_Mc1.controller;
 
+import com.niit.bej.c1_s5_spring_boot_Mc1.domain.Product;
 import com.niit.bej.c1_s5_spring_boot_Mc1.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
@@ -11,5 +19,19 @@ public class ProductController {
     @Autowired
     public ProductController(ProductService productService) {
         this.productService = productService;
+    }
+
+    @GetMapping("/getAllProduct")
+    public ResponseEntity<?> getAllProduct() {
+        List<Product> productList = productService.getAllProduct();
+        if (productList.isEmpty()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        else return new ResponseEntity<>(productList, HttpStatus.FOUND);
+    }
+
+    @PostMapping("/saveProduct")
+    public ResponseEntity<?> saveProduct(@RequestBody Product product) {
+        Product addProduct = productService.addProduct(product);
+        if (addProduct != null) return new ResponseEntity<>(addProduct, HttpStatus.CREATED);
+        else return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
     }
 }
