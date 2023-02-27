579eb8c16693d3fe720712cbb7ce8501ca62de29
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-27 23:39:01 +0530

    Added dependency injection of ProductService class

--- src/main/java/com/niit/bej/c1_s5_spring_boot_Mc1/controller/ProductController.java
+++ src/main/java/com/niit/bej/c1_s5_spring_boot_Mc1/controller/ProductController.java
@@ -1,7 +1,15 @@
 package com.niit.bej.c1_s5_spring_boot_Mc1.controller;
 
+import com.niit.bej.c1_s5_spring_boot_Mc1.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
+    ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 }
