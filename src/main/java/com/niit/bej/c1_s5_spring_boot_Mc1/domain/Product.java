e3728d3fe62835a070979c7ebbbd1ecbeda00843
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-27 23:15:31 +0530

    Created Fields for the class product and annotate the class with @Entity and made productId as primary key

--- src/main/java/com/niit/bej/c1_s5_spring_boot_Mc1/domain/Product.java
+++ src/main/java/com/niit/bej/c1_s5_spring_boot_Mc1/domain/Product.java
@@ -1,4 +1,12 @@
 package com.niit.bej.c1_s5_spring_boot_Mc1.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Product {
+    @Id
+    private int productId;
+    private String productName;
+    private String manufacturer;
 }
